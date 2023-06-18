82ac1e3783de54cfd6180d6be4a55ae73bc2e565
Rizkyemha <rizkymh31@gmail.com>
2023-06-19 01:36:10 +0700

    Mempernaiki config DB

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const DBCONFIG = {
-    host: 'process.env.HOST',
-    port: 'process.env.DB_PORT',
-    database: 'process.env.DATABASE',
-    user: 'process.env.USER',
-    password: 'process.env.PASSWORD',
-    url: 'process.env.URL',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 8061,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    url: process.env.DB_URL,
 }
 
 module.exports = DBCONFIG;
