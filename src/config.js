18508f195c9f7a51d20fa04a5eea1f62fa4f12cb
Rizkyemha <rizkymh31@gmail.com>
2023-06-19 00:53:24 +0700

    Memperbaiki Server DB

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 const DBCONFIG = {
-    host: 'localhost',
+    host: 'containers-us-west-34.railway.app',
     user: 'root',
-    password: 'Genarok-tb1',
-    database: 'berapajasa',
+    password: 'l96397aaLrMbuhLV9N0h',
+    database: 'railway',
+    port: '8061',
+    url: 'mysql://root:l96397aaLrMbuhLV9N0h@containers-us-west-34.railway.app:8061/railway',
 }
 
 module.exports = DBCONFIG;
